167291d056f4024349d6909c1c77cbe63de4c5f9
barnackles <p.martewicz@yahoo.com>
2023-05-25 19:58:55 +0200

    Added exception Handling.

--- src/main/java/com/kodilla/webflux/FirstFluxCheck.java
+++ src/main/java/com/kodilla/webflux/FirstFluxCheck.java
@@ -8,8 +8,12 @@ public class FirstFluxCheck {
         Book b1 = new Book("Title1", "Author1", 2000);
         Book b2 = new Book("Title2", "Author2", 2001);
         Flux<Book> bookFlux = Flux.just(b1, b2);
-        bookFlux.subscribe(System.out::println);
+        bookFlux = bookFlux.concatWith(Flux.error(new Exception("Test exception")));
+        bookFlux.subscribe(System.out::println, FirstFluxCheck::handleException);
+    }
 
+    private static void handleException(Throwable e) {
+        System.out.println("There was an error: " + e);
     }
 
 }
