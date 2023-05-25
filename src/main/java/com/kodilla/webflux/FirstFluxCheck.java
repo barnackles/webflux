8107f90f984f11f0b7863ac5790a6786d372db69
barnackles <p.martewicz@yahoo.com>
2023-05-25 19:57:18 +0200

    Book and FirstFluxCheck classes added.

---
+++ src/main/java/com/kodilla/webflux/FirstFluxCheck.java
@@ -0,0 +1,15 @@
+package com.kodilla.webflux;
+
+import reactor.core.publisher.Flux;
+
+public class FirstFluxCheck {
+
+    public static void main(String[] args) {
+        Book b1 = new Book("Title1", "Author1", 2000);
+        Book b2 = new Book("Title2", "Author2", 2001);
+        Flux<Book> bookFlux = Flux.just(b1, b2);
+        bookFlux.subscribe(System.out::println);
+
+    }
+
+}
